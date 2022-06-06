437de3c4560c49b367820b8a88d067b42a0f98f6
Alastair Codd <alastaircodd@hotmail.co.uk>
2022-06-06 09:45:04 +0100

    Parse games endpoint and store in database
    A random (last in chron data) value for the start time is stored
    in the database, keyed by [season, day]. Values from gamma or the
    coffee cup are currently discarded.
    
    Data is currently unused, but is intended for use for annotation
    of idol board data, and for improved navigation.
    
    Currently using cached data containing all of chron/v1/games --
    possible that this could pull from chron, and probably should if
    the idol board comes back and we want dynamic data capture.
    
    Sled DB is overwritten on every liftoff, which is costly when no
    new updates are occuring.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,97 @@
 use askama::Template;
+// use reqwest::Client;
 use rocket::fairing::AdHoc;
 use rocket::http::ContentType;
 use rocket::response::content::RawHtml;
 use rocket::response::Debug;
 use rocket::{get, launch, routes};
 use serde::Deserialize;
+use sled::Db;
 use std::fs;
+
+lazy_static::lazy_static! {
+    static ref DB: Db = sled::Config::default()
+        .path(std::env::var_os("CARDSTOCK_SLED_V1").expect("CARDSTOCK_SLED_V1 not set in environment"))
+        .use_compression(true)
+        .open()
+        .unwrap();
+
+    // static ref CLIENT: Client = Client::builder()
+        // .user_agent("cardstock/0.0 (lofty@sibr.dev)")
+        // .build()
+        // .unwrap();
+}
+
+// const CHRONICLER_BASE: &str = "https://api.sibr.dev/chronicler/";
+const GAMES_TREE: &str = "games_v1";
+const NUMBER_OF_DAYS_FOR_SEASON_TREE: &str = "number_of_days_for_season_v1";
+
+const ZEROTH_SEASON_WITH_IDOL_BOARD: i16 = 4;
+
+async fn start_task() -> Result<(), sled::Error> {
+    let game_tree = DB.open_tree(GAMES_TREE)?;
+    let number_of_days_for_season_tree = DB.open_tree(NUMBER_OF_DAYS_FOR_SEASON_TREE)?;
+
+    fn keep_greater_merge(
+        _key: &[u8],
+        old_value: Option<&[u8]>,
+        new_value: &[u8],
+    ) -> Option<Vec<u8>> {
+        if let Some(current_value) = old_value {
+            if current_value[0] > new_value[0] {
+                Some(current_value.to_vec())
+            } else {
+                Some(new_value.to_vec())
+            }
+        } else {
+            Some(new_value.to_vec())
+        }
+    }
+
+    number_of_days_for_season_tree.set_merge_operator(keep_greater_merge);
+
+    // let games: GameData = CLIENT
+    // .get(format!("{}/v1/games", CHRONICLER_BASE))
+    // .send()
+    // .await?
+    // .json()
+    // .await?;
+    let contents = fs::read_to_string("games.json").unwrap();
+
+    let games: GameData = serde_json::from_str(&contents).unwrap();
+    for game in games.data.into_iter() {
+        if game.data.sim.is_none() && game.data.season > ZEROTH_SEASON_WITH_IDOL_BOARD {
+            if let Some(start_time) = game.start_time {
+                let data = game.data;
+                let mut key = data.season.to_be_bytes().to_vec();
+                key.extend(data.day.to_be_bytes());
+                game_tree.insert(&key, start_time.as_bytes())?;
+
+                number_of_days_for_season_tree
+                    .merge(data.season.to_be_bytes(), (data.day + 1).to_be_bytes())?;
+            }
+        }
+    }
+
+    Ok(())
+}
 
 type ResponseResult<T> = std::result::Result<T, Debug<anyhow::Error>>;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, css, cardstock, manifest,])
+    rocket::build()
+        .mount("/", routes![index, season, css, cardstock, manifest,])
+        .attach(AdHoc::on_liftoff("Background tasks", |_rocket| {
+            Box::pin(async {
+                match start_task().await {
+                    Err(err) => {
+                        log::error!("{:#}", err);
+                    }
+                    _ => (),
+                }
+            })
+        }))
 }
 
 #[get("/")]
@@ -78,7 +158,29 @@ pub fn manifest() -> (ContentType, &'static str) {
 }
 
 #[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
+struct GameData {
+    data: Vec<Chron1Versions>,
+}
+
+#[derive(Deserialize)]
+struct Chron1Versions {
+    #[serde(rename = "startTime")]
+    start_time: Option<String>,
+    #[serde(rename = "endTime")]
+    end_time: Option<String>,
+    data: Game,
+}
+
+#[derive(Deserialize)]
+struct Game {
+    #[serde(alias = "_id")]
+    id: String,
+    day: u8,
+    season: i16,
+    sim: Option<String>,
+}
+
+#[derive(Deserialize)]
 struct IdolsData {
     items: Vec<ChronV2Versions>,
 }
