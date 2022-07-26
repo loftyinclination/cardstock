716ea8ce806d790817aead8d3d1f33772ada619c
Alastair Codd <alastaircodd@hotmail.co.uk>
2022-07-26 13:49:48 +0100

    Split index page into era grouping

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -5,18 +5,41 @@ use rocket::response::content::RawHtml;
 
 #[get("/")]
 pub fn index() -> ResponseResult<RawHtml<String>> {
-    let seasons = vec![
-        6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    let expansion_seasons = vec![
+        12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
     ];
-    let html_content = IndexPage { seasons }
+
+    let expansion = Era {
+        name: "Expansion",
+        seasons: expansion_seasons,
+    };
+
+    let discipline_seasons = vec![
+        6, 7, 8, 9, 10, 11,
+    ];
+
+    let discipline = Era {
+        name: "Discipline",
+        seasons: discipline_seasons,
+    };
+
+    let eras = vec![
+        expansion, discipline
+    ]
+    let html_content = IndexPage { eras }
         .render()
         .map_err(anyhow::Error::from)
         .unwrap();
     Ok(RawHtml(html_content))
 }
 
+struct Era {
+    name: String,
+    seasons: Vec<i8>,
+}
+
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexPage {
-    seasons: Vec<i8>,
+    eras: Vec<Era>,
 }
